441461479e1bb8f708c105ee66d8de0bc56663b2
Lukas Frick <fricklukas96@gmail.com>
2025-05-16 18:44:35 -0300

    Projeto Parte 1 Finalizado

---
+++ src/styles.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  branca: '#EEEEEE',
+  preto: '#111',
+  cinza: '#333',
+  verde: '#10AC84',
+  cinzaClaro: '#a3a3a3'
+}
+
+const GlobalCss = createGlobalStyle`
+  * {
+    margin:0;
+    padding: 0;
+    box-sizing:border-box;
+    font-family: Roboto, 'sans-serif';
+    list-style: none;
+  }
+
+  body {
+    background-color: ${cores.preto};
+    color: ${cores.branca};
+    padding-top: 40px;
+  }
+
+  .container {
+    max-width: 1024px;
+    witdh: 100%;
+    margin: 0 auto;
+  }
+`
+
+export default GlobalCss
